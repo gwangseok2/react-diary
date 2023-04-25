a1dead5bb5d9a6bc1fe9113c8816d0c1aef36abf
gs.song <rhrnrkd135@gmail.com>
2023-04-25 16:44:16 +0900

    [ + ] 일기 없을 땐 리스트 숨김

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate} />
-      <DiaryList diaryList={data} onRemove={onRemove} onUpdate={onUpdate} />
+      {data.length < 1 ? <></> : <DiaryList diaryList={data} onRemove={onRemove} onUpdate={onUpdate} />}
     </div>
   );
 }
