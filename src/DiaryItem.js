0f907740621029661a214fc25c873be13cfb4670
gs.song <rhrnrkd135@gmail.com>
2023-08-18 18:50:55 +0900

    [ + ] prettier

--- src/DiaryItem.js
+++ src/DiaryItem.js
@@ -1,9 +1,8 @@
-import React, { useContext, useRef, useState } from "react";
-import { DiaryDispatchContext } from "./App";
+import React, { useContext, useRef, useState } from 'react';
+import { DiaryDispatchContext } from './App';
 
 const DiaryItem = ({ author, contents, createDttm, emotion, id }) => {
-
-  const { onRemove, onUpdate } = useContext(DiaryDispatchContext)
+  const { onRemove, onUpdate } = useContext(DiaryDispatchContext);
   const [localContent, setLocalContent] = useState(contents);
   const localContentInput = useRef();
 
@@ -11,21 +10,20 @@ const DiaryItem = ({ author, contents, createDttm, emotion, id }) => {
     if (window.confirm(`${id}번째 일기를 삭제하시겠습니까?`)) {
       onRemove(id);
     }
-  }
+  };
 
   const [isEdit, setIsEdit] = useState(false);
   const toggleIsEdit = () => {
     setIsEdit(!isEdit);
     setTimeout(() => {
       localContentInput.current.focus();
-    })
-  }
+    });
+  };
 
   const cancleEdit = () => {
     setIsEdit(false);
     setLocalContent(contents);
-  }
-
+  };
 
   const handleUpdate = () => {
     if (localContent.length < 5) {
@@ -34,29 +32,62 @@ const DiaryItem = ({ author, contents, createDttm, emotion, id }) => {
     }
 
     if (window.confirm('일기를 수정 하시겠습니까?')) {
-      onUpdate(id, localContent)
+      onUpdate(id, localContent);
       setIsEdit(false);
     }
-  }
-
+  };
 
   return (
     <ul className="DiaryItem">
       <li>{author}의 일기</li>
-      <li><span>{isEdit ? <textarea ref={localContentInput} value={localContent} onChange={(e) => { setLocalContent(e.target.value) }} placeholder="수정 할 텍스트를 입력해주세요." /> : contents}</span></li>
+      <li>
+        <span>
+          {isEdit ? (
+            <textarea
+              ref={localContentInput}
+              value={localContent}
+              onChange={(e) => {
+                setLocalContent(e.target.value);
+              }}
+              placeholder="수정 할 텍스트를 입력해주세요."
+            />
+          ) : (
+            contents
+          )}
+        </span>
+      </li>
       <li>작성일자 : {new Date(createDttm).toLocaleString()}</li>
       <li>감정점수 : {emotion} </li>
       <li>
-        {isEdit ? (<><button type="button" onClick={cancleEdit}>수정취소</button><button type="button" onClick={handleUpdate} style={{ marginLeft: "6px" }}>수정완료</button></>)
-          : (
-            <>
-              <button type="button" onClick={handleRemove}>삭제하기</button>
-              <button type="button" style={{ marginLeft: "6px" }} onClick={toggleIsEdit}>수정하기</button>
-            </>
-          )
-        }
+        {isEdit ? (
+          <>
+            <button type="button" onClick={cancleEdit}>
+              수정취소
+            </button>
+            <button
+              type="button"
+              onClick={handleUpdate}
+              style={{ marginLeft: '6px' }}
+            >
+              수정완료
+            </button>
+          </>
+        ) : (
+          <>
+            <button type="button" onClick={handleRemove}>
+              삭제하기
+            </button>
+            <button
+              type="button"
+              style={{ marginLeft: '6px' }}
+              onClick={toggleIsEdit}
+            >
+              수정하기
+            </button>
+          </>
+        )}
       </li>
-    </ul >
-  )
-}
+    </ul>
+  );
+};
 export default React.memo(DiaryItem);
